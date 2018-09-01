16db4337fafb99b7be6e12d7f4e72b7dd476e424
Ikramunnesa <ikraafi@gmail.com>
2018-09-02 00:00:22 +0600

    modified

--- IST_Pro_1/Form1.cs
+++ IST_Pro_1/Form1.cs
@@ -12,6 +12,7 @@ namespace IST_Pro_1
 {
     public partial class Form1 : Form
     {
+        Presentation.frmCountry country = new Presentation.frmCountry();
         public Form1()
         {
             InitializeComponent();
@@ -20,6 +21,15 @@ namespace IST_Pro_1
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void countryToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (country.IsDisposed)
+                country = new Presentation.frmCountry();
+            country.MdiParent = this;
+            country.Show();
+            country.BringToFront();
         }
     }
 }
